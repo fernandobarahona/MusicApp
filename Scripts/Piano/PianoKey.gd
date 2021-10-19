2bf6bb84427ce604186450fbf811316222c6a3f5
unknown <desiretchinda10@gmail.com>
2021-10-19 16:28:35 +0100

    check success/failure on key pressed

---
+++ Scripts/Piano/PianoKey.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+export(GameManager.Notes) var key_note
+
+
+export(Color) var btn_color
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
